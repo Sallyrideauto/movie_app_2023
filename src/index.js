efd7dafcc501d64edbfd513dd6f33247cd07a331
Sallyrideauto <riru16@knou.ac.kr>
2023-05-07 19:27:52 +0900

    최종 완성 코드

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// React v18 이후 버전에서는 react-dom/client 모듈에서 불러와야 함
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
