d4350e1c678f8f8b28b7683880e3dafffd8c6af0
Sallyrideauto <riru16@knou.ac.kr>
2023-05-07 19:30:08 +0900

    Updates

---
+++ precache-manifest.9232571d429ac873576e26e9114f21fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "818f14cc810a3cf9a370",
+    "url": "/movie_app_2023/static/css/main.b1d21107.chunk.css"
+  },
+  {
+    "revision": "818f14cc810a3cf9a370",
+    "url": "/movie_app_2023/static/js/main.818f14cc.chunk.js"
+  },
+  {
+    "revision": "702a22ebdb7a50abb41a",
+    "url": "/movie_app_2023/static/js/1.702a22eb.chunk.js"
+  },
+  {
+    "revision": "117638157a92709a0ac4",
+    "url": "/movie_app_2023/static/js/runtime~main.11763815.js"
+  },
+  {
+    "revision": "937ed3688171eaa7f1812b68b16d63ab",
+    "url": "/movie_app_2023/index.html"
+  }
+];
